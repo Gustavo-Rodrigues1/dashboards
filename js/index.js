2f68724221175d7125f312595051155678af5a57
Gustavo-Rodrigues1 <gustavo38072015@gmail.com>
2025-09-28 22:08:36 -0300

    style.css: fix toggle year position index.js: add toggle year logic and optimize organization index.html: add footer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
+const _elements = {
+    themeSwitch: document.getElementById('theme-switch'),
+    hamburguerMenu: document.querySelector(".mobile-nav-list"),
+    sideBar: document.querySelector(".side-bar"),
+    yearSelectToggle: document.querySelector(".year-select-toggle"),
+    selectToggleIcon: document.querySelector(".year-select-toggle__icon"),
+    yearSelectToggleSelected: document.querySelector(".year-select-toggle__selected-year"),
+    yearSelectList: document.querySelector(".year-select-list"),
+    selectOptions: document.querySelectorAll(".year-select-list__item")
+}
+
 let darkmode = localStorage.getItem('darkmode')
-const themeSwitch = document.getElementById('theme-switch')
 
 const enableDarkmode = () => {
     document.body.classList.add('darkmode')
@@ -15,14 +25,39 @@ const disableDarkmode = () => {
     localStorage.setItem('darkmode', 'false')
 }
 
-themeSwitch.addEventListener("click", () => {
+_elements.themeSwitch.addEventListener("click", () => {
     darkmode = localStorage.getItem('darkmode')
     darkmode !== "true" ? enableDarkmode() : disableDarkmode()
 })
 
-const hamburguerMenu = document.querySelector(".mobile-nav-list")
-const sideBar = document.querySelector(".side-bar")
-
-hamburguerMenu.addEventListener("click", () => {
-    sideBar.classList.toggle("show");
+_elements.hamburguerMenu.addEventListener("click", () => {
+    _elements.sideBar.classList.toggle("show");
 })
+
+_elements.yearSelectToggle.addEventListener("click", () => {
+    _elements.selectToggleIcon.classList.toggle("year-select-toggle__selected-year--rotate")
+    _elements.yearSelectList.classList.toggle("year-select-list--show")
+})
+
+_elements.selectOptions.forEach(item => {
+    item.addEventListener("click", () => {
+        _elements.yearSelectToggleSelected.innerText = item.innerText
+        _elements.yearSelectToggle.dispatchEvent(new Event("click"));
+    })
+})
+
+const request = async (year) => {
+    try{
+    const urlDrivers = `https://api.jolpi.ca/ergast/f1/${year}/drivers/`
+    const urlTeams = `https://api.jolpi.ca/ergast/f1/${year}/teams/`
+
+    const dataDrivers = await fetch(urlDrivers)
+    const dataTeams= await fetch(urlTeams)
+    const jsonDrivers = await dataDrivers.json
+    const jsonTeams = await dataTeams.json
+
+    return jsonDrivers, jsonTeams
+    } catch(e) {
+        console.log(e)
+    }
+}
