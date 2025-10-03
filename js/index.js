0d65f600f7b220fca0544dd8b8b6097016bddd81
Gustavo-Rodrigues1 <gustavo38072015@gmail.com>
2025-10-02 22:55:20 -0300

    Fixed image position Add slider

--- js/index.js
+++ js/index.js
@@ -129,3 +129,21 @@ const request = async (year) => {
     console.log(error);
   }
 };
+
+const swiper = new Swiper('.card-wrapper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
